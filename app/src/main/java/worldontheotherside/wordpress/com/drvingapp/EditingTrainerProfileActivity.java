e014c11a5a0af773f65af84627fb1bab39a5fa20
wafaMamari <wafaMamari@github.com>
2017-12-03 19:03:00 +0400

    3/12/2017

--- app/src/main/java/worldontheotherside/wordpress/com/drvingapp/EditingTrainerProfileActivity.java
+++ app/src/main/java/worldontheotherside/wordpress/com/drvingapp/EditingTrainerProfileActivity.java
@@ -3,11 +3,11 @@ package worldontheotherside.wordpress.com.drvingapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class TrainerProfileActivity extends AppCompatActivity {
+public class EditingTrainerProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_trainer_profile);
+        setContentView(R.layout.activity_editing_trainer_profile);
     }
 }
