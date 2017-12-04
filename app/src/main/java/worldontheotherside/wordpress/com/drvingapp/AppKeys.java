e7860c71610a13a65bce3cc93abff9f977b5a8c6
Zainab <zainab.yazahraa@gmail.com>
2017-12-04 19:32:58 +0400

    14\11

---
+++ app/src/main/java/worldontheotherside/wordpress/com/drvingapp/AppKeys.java
@@ -0,0 +1,13 @@
+package worldontheotherside.wordpress.com.drvingapp;
+
+/**
+ * Created by زينب on 12/3/2017.
+ */
+
+public class AppKeys {
+    // This class is for the strings, ints, booleans, etc... that will be used in the app frequently
+
+    public static String PREV_TRAINEE = "previous trainee";
+    public static String NEW_TRAINEE = "new trainee";
+    public static String INSTRUCTOR = "instructor";
+}
