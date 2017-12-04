622e0310e3f3ccd78cad08dd765342a82b99bef0
wafaMamari <sunshine102030@outlook.com>
2017-12-04 14:43:41 +0400

    4/12/2017

---
+++ app/src/main/java/worldontheotherside/wordpress/com/drvingapp/Fragments/TrainerProfileFragment.java
@@ -0,0 +1,14 @@
+package worldontheotherside.wordpress.com.drvingapp.Fragments;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TrainerProfileFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
